53a24176f91e5d7f4824fcbb8989a3e1366c4c56
Mat Kelcey <matthew.kelcey@gmail.com>
2020-08-09 21:00:11 +1000

    move away from object for time being

--- random_embeddings/ensemble_net.py
+++ random_embeddings/ensemble_net.py
@@ -23,49 +23,48 @@ def conv_block(stride, with_relu, input, kernel):
     return block
 
 
-class EnsembleNet(object):
+def initial_params(num_models):
+    key = random.PRNGKey(0)
+    subkeys = random.split(key, 8)
 
-    def __init__(self, num_models):
-        self.num_models = num_models
-        key = random.PRNGKey(0)
-        subkeys = random.split(key, 8)
-        self.conv_kernels = []
+    # conv stack
+    conv_kernels = []
+    input_size = 3
+    for i, output_size in enumerate([32, 64, 64, 64, 64, 64]):
+        conv_kernels.append(orthogonal()(
+            subkeys[i], (num_models, 3, 3, input_size, output_size)))
+        input_size = output_size
+    # dense kernel
+    dense_kernels = orthogonal()(subkeys[6], (num_models, 1, 1, 64, 32))
+    # embeddings
+    embedding_kernels = orthogonal()(subkeys[7], (num_models, 1, 1, 32, 32))
 
-        input_size = 3
-        for i, output_size in enumerate([32, 64, 64, 64, 64, 64]):
-            self.conv_kernels.append(orthogonal()(
-                subkeys[i], (num_models, 3, 3, input_size, output_size)))
-            input_size = output_size
+    # return all params
+    return conv_kernels + [dense_kernels, embedding_kernels]
 
-        self.dense_kernels = orthogonal()(
-            subkeys[6], (num_models, 1, 1, 64, 32))
-        self.embedding_kernels = orthogonal()(
-            subkeys[7], (num_models, 1, 1, 32, 32))
 
-    def embed(self, input):
-        # TODO decorating this method directly fails?
-        return jit(self._embed)(input)
+@jit
+def embed(params, input):
+    assert len(params) == 8
+    conv_kernels = params[0:6]
+    dense_kernels = params[6]
+    embedding_kernels = params[7]
 
-    def _embed(self, input):
+    # the first call vmaps over the first kernels for a single input
+    y = vmap(partial(conv_block, 2, True, input))(conv_kernels[0])
 
-        # convolutional stack; stride 2 for downsizing
+    # subsequent calls vmap over both the prior input and the kernel
+    # the first representing the batched input with the second representing
+    # the batched models (i.e. the ensemble)
+    for conv_kernel in conv_kernels[1:]:
+        y = vmap(partial(conv_block, 2, True))(y, conv_kernel)
 
-        # the first call vmaps over the first kernels for a single input
-        y = vmap(partial(conv_block, 2, True, input))(self.conv_kernels[0])
+    # fully convolutional dense layer (with relu) as bottleneck
+    y = vmap(partial(conv_block, 1, True))(y, dense_kernels)
 
-        # subsequent calls vmap over both the prior input and the kernel
-        # the first representing the batched input with the second representing
-        # the batched models (i.e. the ensemble)
-        for conv_kernel in self.conv_kernels[1:]:
-            y = vmap(partial(conv_block, 2, True))(y, conv_kernel)
-
-        # fully convolutional dense layer (with relu) as bottleneck
-        y = vmap(partial(conv_block, 1, True))(y, self.dense_kernels)
-
-        # final projection to embedding dim (with no activation); embeddings are
-        # squeezed and unit length normalised.
-        embeddings = vmap(partial(conv_block, 1, False))(
-            y, self.embedding_kernels)
-        embeddings = jnp.squeeze(embeddings)  # (M, N, 32)
-        embeddings /= jnp.linalg.norm(embeddings, axis=-1, keepdims=True)
-        return embeddings  # (M=models, N=num_inputs, E=embedding_dim)
+    # final projection to embedding dim (with no activation); embeddings are
+    # squeezed and unit length normalised.
+    embeddings = vmap(partial(conv_block, 1, False))(y, embedding_kernels)
+    embeddings = jnp.squeeze(embeddings)  # (M, N, 32)
+    embeddings /= jnp.linalg.norm(embeddings, axis=-1, keepdims=True)
+    return embeddings  # (M=models, N=num_inputs, E=embedding_dim)
